cacee10a247fe2b25f3e111d23bade078581f3b6
David Poduska <poduska@oakland.edu>
2016-09-30 21:10:06 -0400

    Fixed some typos

--- BeerSong.java
+++ BeerSong.java
@@ -7,12 +7,12 @@ public class BeerSong {
 	public static void Ninety_Nine_Bottles_of_Beer(){
 		int count = 99;
 		while(count > 1){
-			System.out.println(count + "bottles of beer on the wall, " + count + " bottles of beer,");
+			System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer,");
 			count--;
-			System.out.println("take one down, pass	it around, " + count + " bottles of beer on the	wall.");
+			System.out.println("take one down, pass it around, " + count + " bottles of beer on the wall.");
 		}		
 		System.out.println("1 bottle of beer on the wall, 1 bottle of beer,");
-		System.out.println("take one down, pass	it around, no more bottles of beer on the wall.");
+		System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
 	}
 
 }
